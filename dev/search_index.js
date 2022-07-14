31c9cbaf3219a7a248303cf1e43a27c5d6fd80dc
Documenter.jl <documenter@juliadocs.github.io>
2022-07-14 01:42:28 +0000

    build based on 94176b7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Basic.jl","page":"Basic.jl","title":"Basic.jl","text":"","category":"section"},{"location":"","page":"Basic.jl","title":"Basic.jl","text":"Documentation for Basic.jl","category":"page"}]
+}
